c197b2d9dfec62e0f3e5d29d601a07f81a9975d0
ANSHxK <154327364+ANSHxK@users.noreply.github.com>
2024-02-15 18:06:50 +0400

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,7 @@
+a=int(input("Enter first number :"))
+b=int(input("Enter second number :"))
+print("The sum of",a, "and" ,b, "is:",a+b)
+print("The subtraction of",a, "and" ,b, "is:",a-b)
+print("The division of",a, "and" ,b, "is:",a/b)
+print("The modulus of",a, "and" ,b, "is:",a%b)
+print("The floor division of",a, "and" ,b, "is:",a//b)
